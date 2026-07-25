ee5d52e5d320b7c8044af5c318fe8455e1ab7a68
Satyajeet Nikam <satyajeetnikam27@gmail.com>
2026-07-25 22:53:28 +0530

    throttle implemented

--- src/App.jsx
+++ src/App.jsx
@@ -7,129 +7,25 @@ import Footer from './Components/Footer'
 import { useRef } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+const lastcall = useRef(0)
+const delay = 2000
 
-let timer = useRef(null);
+  const googleMap = ()=>{
+    const current = Date.now()
+    const diff = current-lastcall.current
+    if(diff >= delay){
+        lastcall.current = current
+        console.log('Load Google Map')
+    }
 
-function search() {
-  clearTimeout(timer);
-
-  timer = setTimeout(() => {
-    console.log("API Call");
-  }, 3000);
-}
-
+  }
+  
   return (
     <>
       <section id="center">
-        <div className="hero">
-
-          <input type="text" onChange={() => search()} />
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started, our company</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
+       <h1>Throttle</h1>
+       <button onClick={googleMap}>Load Google Map</button>
       </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation of the company is legal</h2>
-          <p>Your questions, answered, we respect your value</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Swamini Products
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                We Build Dreams with Passion and Commitment
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-      <Footer/>
     </>
   )
 }
