f4b0241e45c0a27465ed4f91abcb86c73abcca4c
Satyajeet Nikam <satyajeetnikam27@gmail.com>
2026-07-26 08:16:52 +0530

    word is little modified

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const delay = 2000
     <>
       <section id="center">
        <h1>Throttle</h1>
-      <h1>Load Google Map</h1>
+      <h1>Load Google Map API</h1>
       </section>
     </>
   )
