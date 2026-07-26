1b56834b25b20220e8cf6a5fe4dabeaa90f61978
Satyajeet Nikam <satyajeetnikam27@gmail.com>
2026-07-26 07:43:42 +0530

    word changed

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const delay = 2000
     <>
       <section id="center">
        <h1>Throttle</h1>
-       <button onClick={googleMap}>Load Google Map</button>
+       <button onClick={googleMap}>Track Google Map</button>
       </section>
     </>
   )
